95b8618b486e12e5895247aee40d4426fb73bbe7
duongdangduy95 <duy.dd225185@sis.hust.edu.vn>
2025-05-20 17:09:35 +0700

    Push code cảm biến ánh sáng quang trở dùng A0 và D7

--- cambienanhsangquangtro.ino
+++ cambienanhsangquangtro.ino
@@ -4,7 +4,7 @@ int va=0;
 int vd=0;
 int led=13;
 void setup() {
-  // put your setup code here, to run once:
+ 
   pinMode(d,INPUT);
   pinMode(led, OUTPUT);
   Serial.begin(9600);
@@ -12,7 +12,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   va=analogRead(a);
   vd=digitalRead(d);
   Serial.print(va);
